b36bb903de7ad3d417a38789343cb7fd5b16127f
bkrukowski <krukowski.bartlomiej@gmail.com>
2022-07-26 00:11:00 +0400

    Make the defer logic more explicit

--- src/DeferrableStack.php
+++ src/DeferrableStack.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the php-defer/php-defer package.
+ *
+ * (c) Bartłomiej Krukowski <bartlomiej@krukowski.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PhpDefer;
 
 /**
--- src/functions.inc.php
+++ src/functions.inc.php
@@ -1,7 +1,5 @@
 <?php
 
-use PhpDefer\DeferrableStack;
-
 /*
  * This file is part of the php-defer/php-defer package.
  *
@@ -10,6 +8,8 @@ use PhpDefer\DeferrableStack;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+use PhpDefer\DeferrableStack;
 
 /**
  * @param callable $callback
