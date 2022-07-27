7a38eb91360b8c1c728577f4d1c13e249283ea93
Bartłomiej Krukowski <bkrukowski@users.noreply.github.com>
2022-07-27 21:53:44 +0400

    v3.0.5 (#34)

--- src/functions.inc.php
+++ src/functions.inc.php
@@ -12,7 +12,8 @@
 use PhpDefer\DeferrableStack;
 
 /**
- * @param callable $callback
+ * @param null|DeferrableStack $context  the context must be an optional parameter, that is the only way to make it nullable in older PHP versions
+ * @param callable             $callback
  */
 function defer(DeferrableStack &$context = null, $callback)
 {
